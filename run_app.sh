da7a568f25394cd8d21784f313e8a3f5b132a6ee
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-08 22:09:23 +0530

    Add bundle based deployment via volume

--- run_app.sh
+++ run_app.sh
@@ -1,3 +1,15 @@
-cd /built_app
+set -e
+
+if [ -d /bundle ]; then
+  cd /bundle
+  tar xzf *.tar.gz
+  cd /bundle/bundle/programs/server/
+  npm i
+  cd /bundle/bundle/
+else
+  cd /built_app
+fi
+
 export PORT=80
+echo "starting meteor app on port:$PORT"
 node main.js
