45fce43ededf288926e8ea911134b1322e9e2326
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-08 22:30:11 +0530

    Add support for BUNDLE_URL based deployment

--- run_app.sh
+++ run_app.sh
@@ -6,6 +6,13 @@ if [ -d /bundle ]; then
   cd /bundle/bundle/programs/server/
   npm i
   cd /bundle/bundle/
+elif [[ $BUNDLE_URL ]]; then
+  cd /tmp
+  wget $BUNDLE_URL -O bundle.tar.gz
+  tar xzf bundle.tar.gz
+  cd /tmp/bundle/programs/server/
+  npm i
+  cd /tmp/bundle/
 else
   cd /built_app
 fi
