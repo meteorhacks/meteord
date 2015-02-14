c6048d578a7ced3bc9e04d6537a2bd445f70fb6e
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2015-02-15 05:25:03 +0530

    Add a test for binary npm module bulding

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -6,4 +6,5 @@ bash ./build_it.sh
 bash ./test_meteor_app.sh
 bash ./test_bundle_local_mount.sh
 bash ./test_bundle_web.sh
+bash ./test_binary_build.sh
 bash ./test_phantomjs.sh
---
+++ tests/test_binary_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function clean() {
+  docker rm -f binary_build
+}
+
+cd /tmp
+clean
+
+docker run -d \
+    --name binary_build \
+    -e ROOT_URL=http://binary_build_app \
+    -e BUNDLE_URL=https://s3.amazonaws.com/zeema-data/aa.tar.gz \
+    -e REBULD_NPM_MODULES=1 \
+    -p 9090:80 \
+    meteord
+
+sleep 8
+
+appContent=`curl http://localhost:9090`
+clean
+
+if [[ $appContent != *"binary_build_app"* ]]; then
+  echo "Failed: Binary Building"
+  exit 1
+fi
